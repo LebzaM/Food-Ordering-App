33bdb3b7a66d5cc7d3b20c4849e4952c517c516c
Your Name <lebomariri505@gmail.com>
2024-01-06 12:11:03 +0200

    updated

--- utilities/axiosconfig.js
+++ utilities/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://cussospizza.vercel.app/',
+  baseURL: 'https://cussospizza.vercel.app',
 });
 
 export default axiosInstance;
